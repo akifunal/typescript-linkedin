47f9d77ab04b8d4fa84982cf5d427a893f0cb675
akif <akifunal05@gmail.com>
2023-03-26 04:11:22 +0300

    typescript-essential-training: chapter 2 solved

--- TypeScript-Essential-Training/practice/src/demo.ts
+++ TypeScript-Essential-Training/practice/src/demo.ts
@@ -1,5 +1,44 @@
-class Contact {
-    id;
-    name;
-    birthDate;
+interface Contact {
+	id: number
+	name: ContactName
+	birthDate?: Date
+	status: ContactStatus
 }
+
+type ContactName = string
+
+enum ContactStatus {
+	Active = 'active',
+	Inactive = 'inactive',
+	New = 'new',
+}
+
+interface UserContact<TExternalId> {
+	id: number
+	name: string
+	birthDate?: Date
+	status: ContactStatus
+	username: string
+	externalId: TExternalId
+	loadExternalId(): Promise<TExternalId>
+}
+
+function clone<T1, T2 extends T1>(source: T1): T2 {
+	if (typeof source !== 'object' || source === null) {
+		throw new Error('Object expected')
+	}
+
+	return { ...source } as T2
+}
+
+const primaryContact: Contact = {
+	id: 1,
+	name: 'John',
+	birthDate: new Date('01-01-1980'),
+	status: ContactStatus.Active,
+}
+
+const b = clone<Contact, UserContact<string>>(primaryContact)
+
+const dateRange = { startDate: new Date(), endDate: new Date() }
+const dateRangeCopy = clone(dateRange)
